95606589663925a6181597961652457b50f4615e
JeremyR <166337160+jroderiko@users.noreply.github.com>
2025-07-05 15:56:21 -0500

    Update nrinstall.sh

--- nordroll/nrinstall.sh
+++ nordroll/nrinstall.sh
@@ -10,5 +10,6 @@ curl -O https://raw.githubusercontent.com/jroderiko/vlab/main/nordroll/nordl.ls
 curl -O https://raw.githubusercontent.com/jroderiko/vlab/main/nordroll/nroll.sh
 echo "Creating alias..."
 echo "alias nordrl='~/.config/nordroll/nroll.sh' " >> ~/.bashrc
-echo "Approve script to run..."
+echo "Make script executable..."
 sudo chmod +x nroll.sh
+echo "DONE"
