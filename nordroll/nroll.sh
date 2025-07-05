67bb5f364f3a68f6f750478a4eb85016b7ab95ab
JeremyR <166337160+jroderiko@users.noreply.github.com>
2025-07-05 15:53:13 -0500

    Update nroll.sh

--- nordroll/nroll.sh
+++ nordroll/nroll.sh
@@ -2,15 +2,15 @@
 # Script for randomizing NordVPN connection
 #
 # Function to select random location
-function nroll { 
+function nroll {
+	echo "Selecting Location..."
 	linum=$((1 + $RANDOM % $nscope)) # selects random number
-	nloc=$(sed "${linum}q;d" $nlist) # uses random number to pick line from file
-	echo "location: $nloc"
+	nloc=$(sed "${linum}q;d" $1) # uses random number to pick line from file
+	echo $nloc
 }
 
 # Variables
 nloc=" " # selected location
-nlist=" " # path to location list
 nscope=" " # number of lines in file
 
 # Take roll type from arguements
@@ -19,12 +19,12 @@ rolltype="$1"
 # Rolltype determines which list to use
 case $rolltype in
 	l | L)
-		nscope="25"
+		nscope="$(sed -n '$=' ~/.config/nordroll/nordl.ls)"
 		nroll ~/.config/nordroll/nordl.ls
 		;;
 	g | G)
 		
-		nscope="126"
+		nscope="$(sed -n '$=' ~/.config/nordroll/nordg.ls)"
 		nroll ~/.config/nordroll/nordg.ls
 		;;
 	*)
