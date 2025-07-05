6f80ce222b139388a62ab45434c328188e7ed466
JeremyR <166337160+jroderiko@users.noreply.github.com>
2025-07-05 15:25:30 -0500

    Update nrinstall.sh

--- nordroll/nrinstall.sh
+++ nordroll/nrinstall.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo ""
-echo "NordRoll Setup Script v1\n"
-echo "Creating NordRoll Directory"
+echo "*** NordRoll Setup Script v1 ***"
+echo "Creating NordRoll Directory..."
 mkdir ~/.config/nordroll
 cd ~/.config/nordroll
-echo "Copying from Github"
+echo "Copying from Github..."
 curl -O https://raw.githubusercontent.com/jroderiko/vlab/main/nordroll/nordg.ls
 curl -O https://raw.githubusercontent.com/jroderiko/vlab/main/nordroll/nordl.ls
 curl -O https://raw.githubusercontent.com/jroderiko/vlab/main/nordroll/nroll.sh
-echo "Creating alias"
+echo "Creating alias..."
 echo "alias nordrl='~/.config/nordroll/nroll.sh' " >> ~/.bashrc
-echo "Approve script to run"
+echo "Approve script to run..."
 sudo chmod +x nroll.sh
